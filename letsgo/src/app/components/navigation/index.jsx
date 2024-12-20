7e122b20f186e6307f4deda89d99f745d72ef9ac
utsav-baghela <utsavu858@gmail.com>
2024-12-20 17:14:22 +0530

    finding buttons angle to place in circular around the 3d animation

--- letsgo/src/app/components/navigation/index.jsx
+++ letsgo/src/app/components/navigation/index.jsx
@@ -1,17 +1,35 @@
-"use client"
-import { BtnList } from '@/app/data'
-import React from 'react'
+"use client";
+import { BtnList } from "@/app/data";
+import React from "react";
 
 const Navigation = () => {
-  return (
-    <div className='flex items-center justify-between relative'>
-       {
-        BtnList.map((btn, index) =>{
-            return <button key={index }>{btn.label}</button>
-        })
-       }
-    </div>
-  )
-}
+  //calculatin the angle of the button for circular animatio
 
-export default Navigation
+  const angleIncrement = 360 / BtnList.length;
+
+  return (
+    <div className="w-full fixed h-screen flex items-center justify-center">
+        <div className="flex items-center justify-between relative">
+      {BtnList.map((btn, index) => {
+        const angleRad = (index * angleIncrement * Math.PI) / 180;
+        const radius = "calc(20vw - 1rem)";
+        const x = `calc(${radius} * ${Math.cos(angleRad)})`;
+        const y = `calc(${radius} * ${Math.sin(angleRad)})`;
+
+        return (
+          <button
+            key={index}
+            className="absolute"
+            style={{
+              transform: `translate(${x}, ${y})`,
+            }}
+          >{btn.label}
+          </button>
+        );
+      })}
+    </div>
+    </div>
+  );
+};
+
+export default Navigation;
