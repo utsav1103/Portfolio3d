ab140f2c14da2de85bfd616c01b95520e55b07d0
utsav-baghela <utsavu858@gmail.com>
2024-12-21 18:25:35 +0530

    adding fireflies

--- letsgo/src/app/layout.js
+++ letsgo/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import FireFliesBackground from "./components/FireFliesBackground";
 const inter = Inter({ subsets: ["latin"],
   variable: "--font-inter", 
  });
@@ -13,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={clsx(inter.variable, "bg-background text-foreground")}>{children}</body>
+      <body className={clsx(inter.variable, "bg-background text-foreground")}>{children}
+        <FireFliesBackground />
+      </body>
     </html>
   );
 }
