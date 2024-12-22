8388b1a073061f7ebff4303a2fd000a89c41828d
utsav-baghela <utsavu858@gmail.com>
2024-12-22 11:49:20 +0530

    Adding projects on project page and styling them

---
+++ letsgo/src/app/projects/page.js
@@ -0,0 +1,27 @@
+//page for projects
+import Image from "next/image";
+import bg from "../../../public/background/projects-background.png";
+import ProjectList from "../components/projects";
+import { projectsData } from "../data";
+import RenderModel from "../components/RenderModel";
+import Wizard from "../components/models/Wizard";
+
+ 
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+      <Image
+        src={bg}
+        alt="background-image"
+        fill
+        className="w-full h-full object-cover object-center opacity-25"
+      />
+
+      <ProjectList projects={projectsData} /> 
+
+      <RenderModel>
+        <Wizard />
+      </RenderModel>
+    </main>
+  );
+}
