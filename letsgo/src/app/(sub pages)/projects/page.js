ab6ddb388c60a88f49c65b78760671f79b813eb8
utsav-baghela <utsavu858@gmail.com>
2024-12-24 17:16:18 +0530

    adding home button to project page

--- letsgo/src/app/(sub pages)/projects/page.js
+++ letsgo/src/app/(sub pages)/projects/page.js
@@ -1,21 +1,21 @@
 //page for projects
 import Image from "next/image";
-import bg from "../../../public/background/projects-background.png";
-import ProjectList from "../components/projects";
-import { projectsData } from "../data";
-import RenderModel from "../components/RenderModel";
-import Staff from "../components/models/Staff";
+import bg from "../../../../public/background/projects-background.png";
+import { projectsData } from "../../data";
+import ProjectList from "@/app/components/projects";
+import RenderModel from "@/app/components/RenderModel";
+import Staff from "@/app/components/models/Staff";
 
 
  
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between relative">
+    <>
       <Image
         src={bg}
         alt="background-image"
-        fill
-        className="w-full h-full object-cover object-center opacity-25"
+      
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"
       />
 
       <div className="flex-1 ml-[30%] px-8">
@@ -27,6 +27,6 @@ export default function Home() {
         <Staff/>
       </RenderModel>
       </div>
-    </main>
+    </>
   );
 }
---
+++ letsgo/src/app/components/HomeBtn.jsx
@@ -0,0 +1,29 @@
+"use client"
+import { Home } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+const HomeBtn = () => {
+  return (
+    <Link 
+    href={"/"}
+    target={"_self"}
+    className='text-foreground  rounded-full flex items-center justify-center custom-bg fixed left-4 top-4 w-fit self-start'
+
+    aria-label={"home"}
+    name={"home"}
+    
+    >
+        <span className="relative  w-12 h-12 p-4  hover:text-accent">
+            <Home className="w-full h-auto" strokeWidth={1.5} />
+
+            <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
+                Home
+            </span>
+        </span>
+
+    </Link>
+  )
+}
+
+export default HomeBtn;
