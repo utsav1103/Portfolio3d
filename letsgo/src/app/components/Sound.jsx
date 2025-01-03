9f9326a1c66f142367469531a93b1e8b69bf2620
utsav-baghela <utsavu858@gmail.com>
2025-01-03 18:09:36 +0530

    adding the modal to for music

--- letsgo/src/app/components/Sound.jsx
+++ letsgo/src/app/components/Sound.jsx
@@ -2,13 +2,36 @@
 import { motion } from 'framer-motion'
 import { Volume2, VolumeX } from 'lucide-react';
 import React, { useEffect, useRef, useState } from 'react'
+import { createPortal } from 'react-dom';
+
+const Modal = ({onClose , toggle})=> {
+    return createPortal(
+        <div className='fixed inset-0 bg-background/60 backdrop-blur-sm flex items-center justify-center'>
+            
+        <div className='bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] py-8 px-6 xs:px-10 sm:px-16 rounded shadow-glass-inset text-center space-y-8'>
+            <p className='font-light'>Do you want to enable music?</p>
+        
+        <div onClick={toggle} className='flex items-center justify-center space-x-4'>
+        <button className='px-4 py-2 border border-accent/30 border-solid hover:shadow-glass-sm rounded mr-2'>Yes</button>    
 
 
+        <button onClick={onClose} className='px-4 py-2 border border-accent/30 border-solid hover:shadow-glass-sm rounded '>No</button> 
+        </div>        
+        
+        </div>
+
+        </div>,
+
+        document.getElementById('my-modal')
+    )
+}
 
 const Sound = () => {
     const audioRef = useRef(null);
     const [isPlaying, setIsPlaying] = 
     useState(false);
+
+    const [showModal, setShowModal] = useState(false);
 
     const handleFirstUserInteraction = () => {
         const musicConsent = localStorage.getItem("musicConsent");
@@ -29,6 +52,8 @@ const Sound = () => {
                 ['click', 'keydown', 'touchstart'].forEach((event) => {
                     document.addEventListener(event, handleFirstUserInteraction);
                 });
+            }else{
+                setShowModal(true);
             }
             
         }
@@ -39,11 +64,18 @@ const Sound = () => {
         setIsPlaying(!isPlaying);
         !newState ? audioRef.current.play() : audioRef.current.pause();
         localStorage.setItem("musicConsent", String(newState));
+        setShowModal(false);
     }
 
 
   return (
     <div className='fixed top-4 right-2.5 xs:right-5 z-50 group'>
+
+    {
+        showModal && <Modal onClose={() => setShowModal(false)} toggle={toggle}
+        />   
+    }
+
         <audio ref={audioRef} loop>
             <source src={"/audio/birds39-forest-20772.mp3"} type='audio/mpeg' />
             your browser does not support audio
--- letsgo/src/app/layout.js
+++ letsgo/src/app/layout.js
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className={clsx(inter.variable, "bg-background text-foreground font-inter")}>{children}
         <FireFliesBackground />
         <Sound />
+        <div id="my-modal"/>
       </body>
     </html>
   );
