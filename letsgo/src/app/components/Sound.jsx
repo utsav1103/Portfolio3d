e1a33c4148a1c957da0ed16d4bdcb3700ded34ec
utsav-baghela <utsavu858@gmail.com>
2025-01-03 21:48:08 +0530

    some changes in sound button

--- letsgo/src/app/components/Sound.jsx
+++ letsgo/src/app/components/Sound.jsx
@@ -11,8 +11,8 @@ const Modal = ({onClose , toggle})=> {
         <div className='bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] py-8 px-6 xs:px-10 sm:px-16 rounded shadow-glass-inset text-center space-y-8'>
             <p className='font-light'>Do you want to enable music?</p>
         
-        <div onClick={toggle} className='flex items-center justify-center space-x-4'>
-        <button className='px-4 py-2 border border-accent/30 border-solid hover:shadow-glass-sm rounded mr-2'>Yes</button>    
+        <div  className='flex items-center justify-center space-x-4'>
+        <button onClick={toggle} className='px-4 py-2 border border-accent/30 border-solid hover:shadow-glass-sm rounded mr-2'>Yes</button>    
 
 
         <button onClick={onClose} className='px-4 py-2 border border-accent/30 border-solid hover:shadow-glass-sm rounded '>No</button> 
