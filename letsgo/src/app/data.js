5605c8bc4eb6051a66c034bdb2d8fd628cf92154
utsav1103 <utsavu858@gmail.com>
2026-01-06 21:18:13 +0530

    listing project

--- letsgo/src/app/data.js
+++ letsgo/src/app/data.js
@@ -17,108 +17,106 @@ Websites:
 */
 
 export const projectsData = [
-    {
-      id: 1,
-      name: "School Management System",
-      description: "Know about the details and admission of your dream school",
-      date: "2025-06-01",
+  {
+    id: 1,
+    name: "School Management System",
+    description: "Know about the details and admission of your dream school",
+    date: "2025-06-01",
     demoLink: "https://github.com/utsav1103/School_Management_System.git",
-    },
+  }, {
+    id: 2,
+    name: "AI Resume Analyzer",
+    description: "Analyze your resume with AI",
+    date: "2022-04-18",
+    demoLink: "https://github.com/utsav1103/ai-resume-analyzer.git",
+  },
+    
     {
-      id: 2,
-      name: "AI writing Assistant",
-      description: "Get it written by your AI",
-      date: "2025-03-25",
-      demoLink: "https://github.com/utsav1103/AI_writing_assistant.git",
-    },
-    {
-      id: 3,
-      name: "3D Portfolio",
-      description: "Amazing 3d portfolio with Magical theme",
-      date: "2025-01-04",
-      demoLink: "https://github.com/utsav1103/Portfolio3d.git",
-    },
-    {
-      id: 4,
-      name: "Favorite places app",
-      description: "Visit your fav places and take a view online",
-      date: "2023-09-25",
-      demoLink: "https://github.com/utsav1103/favorite_places_app.git",
-    },
-    {
-      id: 5,
-      name: "Chat app",
-      description: "Chat with your friends",
-      date: "2023-10-11",
-      demoLink: "https://github.com/utsav1103/ub_chat_app.git",
-    },
-    {
-      id: 6,
-      name: "Blogging web app",
-      description: "Post your blogs and share with friends",
-      date: "2024-9-30",
-      demoLink: "https://github.com/utsav1103/Web_dev_basic/tree/afe7aeb28aa81fc405c7276e7995a20acbdc5101/Projects/Blog-project",
-    },
-    {
-      id: 7,
-      name: "AI Resume Analyzer",
-      description: "Analyze your resume with AI",
-      date: "2022-04-18",
-      demoLink: "https://github.com/utsav1103/ai-resume-analyzer.git",
-    },
-    {
-      id: 8,
-      name: "TechTalk",
-      description: "Tech news aggregator",
-      date: "2022-11-05",
-      demoLink: "https://techtalk.example.com",
-    },
-    {
-      id: 9,
-      name: "FitTrack",
-      description: "Fitness and workout tracker",
-      date: "2022-03-22",
-      demoLink: "https://fittrack.example.com",
-    },
-    {
-      id: 10,
-      name: "MindfulMoments",
-      description: "Meditation and mindfulness app",
-      date: "2022-02-14",
-      demoLink: "https://mindfulmoments.example.com",
-    },
-  ];
-  
-  export const BtnList = [
-    { label: "Home", link: "/", icon: "home", newTab: false },
-    { label: "About", link: "/about", icon: "about", newTab: false },
-    { label: "Projects", link: "/projects", icon: "projects", newTab: false },
-    { label: "Contact", link: "/contact", icon: "contact", newTab: false },
-    {
-      label: "Github",
-      link: "https://www.github.com/utsav1103",
-      icon: "github",
-      newTab: true,
-    },
-    {
-      label: "LinkedIn",
-      link: "https://www.linkedin.com/in/utsav-vaghela11",
-      icon: "linkedin",
-      newTab: true,
-    },
-    {
-      label: "X",
-      link: "https://www.x.com/utsavu858",
-      icon: "twitter",
-      newTab: true,
-    },
-    {
-      label: "Resume",
-      link: "",
-      icon: "resume",
-      newTab: true,
-    },
-  ];
-  
-  
-  
+    id: 3,
+    name: "3D Portfolio",
+    description: "Amazing 3d portfolio with Magical theme",
+    date: "2025-01-04",
+    demoLink: "https://github.com/utsav1103/Portfolio3d.git",
+  },
+  {
+    id: 4,
+    name: "Favorite places app",
+    description: "Visit your fav places and take a view online",
+    date: "2023-09-25",
+    demoLink: "https://github.com/utsav1103/favorite_places_app.git",
+  },
+  {
+    id: 5,
+    name: "Chat app",
+    description: "Chat with your friends",
+    date: "2023-10-11",
+    demoLink: "https://github.com/utsav1103/ub_chat_app.git",
+  },
+  {
+    id: 6,
+    name: "Blogging web app",
+    description: "Post your blogs and share with friends",
+    date: "2024-9-30",
+    demoLink: "https://github.com/utsav1103/Web_dev_basic/tree/afe7aeb28aa81fc405c7276e7995a20acbdc5101/Projects/Blog-project",
+  }, {
+    id: 7,
+    name: "AI writing Assistant",
+    description: "Get it written by your AI",
+    date: "2025-03-25",
+    demoLink: "https://github.com/utsav1103/AI_writing_assistant.git",
+  },
+  {
+    id: 8,
+    name: "TechTalk",
+    description: "Tech news aggregator",
+    date: "2022-11-05",
+    demoLink: "https://techtalk.example.com",
+  },
+  {
+    id: 9,
+    name: "FitTrack",
+    description: "Fitness and workout tracker",
+    date: "2022-03-22",
+    demoLink: "https://fittrack.example.com",
+  },
+  {
+    id: 10,
+    name: "MindfulMoments",
+    description: "Meditation and mindfulness app",
+    date: "2022-02-14",
+    demoLink: "https://mindfulmoments.example.com",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/utsav1103",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/utsav-vaghela11",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://www.x.com/utsavu858",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
